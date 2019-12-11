90830814623f3243d7de0634a1c15cca1b518395
Lucas LABATUT <lucas.labatut@outlook.fr>
2019-12-11 12:22:56 +0100

    BASE SITE V1

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<header>
+  <h1> Le site de Lulu Labatut </h1>
+  <nav>
+    <ul>
+      <li><a href='index.php?page=accueil' title="Retour à la page d'accueil">Accueil</a></li>
+      <li><a href='index.php?page=cv'title="Voici mon CV">CV</a></li>
+      <li><a href='index.php?page=contact'title="Nous contacter">Contact</a></li>
+      <li><a href='index.php?page=galerie'title="La belle galerie">Galerie</a></li>
+    </ul>
+  </nav>
+</header>
--- includes/pages/contact.inc.php
+++ includes/pages/contact.inc.php
@@ -1,24 +1,42 @@
 <h1>Contact</h1>
 
 <!DOCTYPE html>
-<html lang=fr dir="ltr">
+<html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Contact</title>
-      <link rel="stylesheet" href="../../assets/css/style.css">
+    <title><h1>Contact</h1></title>
+    <link rel="stylesheet" href=".\assets/css/reset.css">
   </head>
   <body>
 
-    <div id="wrap2">
-      <nav>
-        <ul>
-          <li><a href="./accueil.inc.php">Accueil</a></li>
-          <li><a href="./contact.inc.php">Contact</a></li>
-          <li><a href="./cv.inc.php">CV</a></li>
-          <li><a href="./galerie.inc.php">Galerie</a></li>
-        </ul>
-      </nav>
-    </div>
-
   </body>
 </html>
+
+<h1>Pour nous contacter</h1>
+<?php
+if (isset($_POST['formcontact'])) {
+  $nom = checkInput($_POST['nom']);
+  $prenom = checkInput($_POST['prenom']);
+  $email = checkInput($_POST['email']);
+  $msg = checkInput($_POST['msg']);
+  $erreur = array();
+  if ($nom === "") {
+    array_push($erreur, "Veuillez saisir votre nom");
+  if ($prenom === "")
+    array_push($erreur, "Veuillez saisir un prénom");
+  if ($email === "")
+    array_push($erreur, "Veuillez saisir une adresse e-mail");
+  if ($msg === "")
+    array_push($erreur, "Veuillez saisir un message");
+}
+  if (count($erreur) > 0) {
+    $message = '<ul>';
+    $message .= '</ul>';
+  }
+  else {
+    echo "Insertion en BDD";
+  }
+}
+  else {
+    require './includes/formcontact.php';
+}
