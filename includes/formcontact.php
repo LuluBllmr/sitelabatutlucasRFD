2adf40b6f611872f05acec06cca562380f920ead
Lucas LABATUT <lucas.labatut@outlook.fr>
2019-12-13 14:41:52 +0100

    Contact

--- includes/formcontact.php
+++ includes/formcontact.php
@@ -1,22 +1,25 @@
 <form action="#" method="post">
+  <fieldset>
+    <legend>Coucou</legend>
   <div>
     <label for="nom">Nom :</label>
     <input type="text" name="nom" id="nom"/>
   </div>
   <div>
-    <label for="prenom">prénom : </label>
+    <label for="prenom">Prénom : </label>
     <input type="text" name="prenom" id="prenom" />
   </div>
   <div>
-    <label for="email">email : </label>
+    <label for="email">Email : </label>
     <input type="email" name="email" id="email" />
   </div>
   <div>
-    <label for="mdp">mot de passe : </label>
-    <input type="password" name="mdp" id="mdp" />
+    <label for="msg">Message : </label>
+    <textarea name="msg" id="msg"><?=$msg ?></textarea>
   </div>
   <div>
     <input type="submit" value="Envoyer" />
   </div>
   <input type="hidden" value="Formulaire" />
+</fieldset>
 </form>
--- includes/pages/contact.inc.php
+++ includes/pages/contact.inc.php
@@ -1,11 +1,9 @@
-<h1>Contact</h1>
-
 <!DOCTYPE html>
-<html lang="fr" dir="ltr">
+<html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title><h1>Contact</h1></title>
-    <link rel="stylesheet" href=".\assets/css/reset.css">
+    <link rel="stylesheet" href="C:\wamp64\www\rfd\assets\css\reset.css">
   </head>
   <body>
 
@@ -14,7 +12,7 @@
 
 <h1>Pour nous contacter</h1>
 <?php
-if (isset($_POST['formcontact'])) {
+if (isset($_POST['formulaire'])) {
   $nom = checkInput($_POST['nom']);
   $prenom = checkInput($_POST['prenom']);
   $email = checkInput($_POST['email']);
@@ -27,22 +25,42 @@ if (isset($_POST['formcontact'])) {
   if ($email === "")
     array_push($erreur, "Veuillez saisir une adresse e-mail");
   if ($msg === "")
-    array_push($erreur, "Veuillez saisir un message");
+    array_push($erreur, "Veuillezl saisir un message");
 }
   if (count($erreur) > 0) {
     $message = '<ul>';
-
-    for ($i=0; $i < count($erreur) ; $i++) {
+    for($i = 0 ; $i < count($erreur) ; $i++) {
       $message .= '<li>';
       $message .= $erreur[$i];
       $message .= '</li>';
     }
-    $message.='</ul>'; 
+    $message .= '</ul>';
+    echo $message;
+    require 'formulaire.php';
   }
   else {
     echo "Insertion en BDD";
   }
 }
-  else {
-    require './includes/formcontact.php';
+else {
+  $nom = $prenom = $email = $msg = "";
+  require 'formulaire.php';
 }
+      $sqlVerif = "SELECT COUNT(*) FROM clients
+      WHERE mail='" . $email ."'";
+      $nbrOccurences = $pdo->query($sqlVerif)->fetchColumn();
+      if ($nbrOccurences > 0) {
+        echo "Déjà dans la base";
+      }
+      else {
+          $sql = "INSERT INTO clients
+          (nom, prenom, mail, message)
+          VALUES ('" . $nom . "', '" . $prenom . "', '" . $email ."', '" . $msg ."')";
+          $query = $pdo->prepare($sql);
+          $query->bindValue('nom', $nom, PDO::PARAM_STR);
+          $query->bindValue('prenom', $prenom, PDO::PARAM_STR);
+          $query->bindValue('email', $email, PDO::PARAM_STR);
+          $query->bindValue('message', $msg, PDO::PARAM_STR);
+          $query->execute();
+          echo "Enregistrement OK";
+        }
